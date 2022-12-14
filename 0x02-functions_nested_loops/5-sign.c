b8cfd9b39f11b1cdaaa1573ed5d5916728e64a08
olaideakinbode <olaideakinbode@gmail.com>
2022-12-13 20:03:38 -0800

    checks a character..

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *_isalpha - checks for alphabetic character
- *@c: character to be checked
- *Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: character to be checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 if otherwise
  */
 int _isalpha(int c)
 {
