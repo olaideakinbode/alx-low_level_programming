e59229cdb7e51da39205db17c8d54833c75fd21e
olaideakinbode <olaideakinbode@gmail.com>
2022-12-13 16:00:14 -0800

    prints alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,10 @@ void print_alphabet_x10(void)
 	int a;
 	char b;
 
-	for (b = 'a'; b <= 'z'; b++)
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
 		_putchar(b);
+	}
 	_putchar('\n');
 }
