9c309825817189d00c10027cd7504e486801aec1
olaideakinbode <olaideakinbode@gmail.com>
2022-12-20 12:21:10 -0800

    string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,41 @@
 #include "main.h"
 
 /**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ * Return: A integer
  *
- *  * _strcpy - copy the string pointed to by src to dest
- *
- *   * @dest: char to check
- *
- *    * @src: char to check
- *
- *     * Return: 0 is success
- *
- *      */
+ */
 
-char *_strcpy(char *dest, char *src)
-
+int _atoi(char *s)
 {
+	int c = 0;
 
-	 int a;
+	unsigned int ni = 0;
 
+	int min = 1;
 
+	int isi = 0;
 
-	  for (a = 0; src[a] != '\0'; a++)
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
+	}
+	ni *= min;
 
-		    dest[a] = src[a];
-
-	   dest[a] = '\0';
-
-	    return (dest);
-
+	return (ni);
 }
