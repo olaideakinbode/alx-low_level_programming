081a32c590a4068f722d62afeb6f44e66f29c509
olaideakinbode <olaideakinbode@gmail.com>
2022-12-20 00:27:32 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: given string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	for (; *s++)
+		counter++
+		return (counter);
+
+}
