62685bc62d7987c5687e82997ce8c545330610bc
olaideakinbode <olaideakinbode@gmail.com>
2022-12-22 07:45:46 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @src: src string
+ * @dest: dest string
+ * @n: int length
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+
+	return (dest);
+}
