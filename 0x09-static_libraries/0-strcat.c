e4197bdce5798cab1893a3ae1998cf80f1494621
olaideakinbode <olaideakinbode@gmail.com>
2023-01-09 13:11:45 -0800

    0x09-static_libraries/2-strchr.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -7,22 +7,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int length, j;
 
-	i = 0;
-	while (dest[i] != '\0')
+	length = 0;
+	while (dest[length] != '\0')
 	{
-		i++;
+		length++;
 	}
-	j = 0;
-
-	while (src[j] != '\0')
+	for (j = 0; src[j] != '\0'; j++, length++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[length] = src[j];
 	}
-	dest[i] = '\0';
+	dest[length] = '\0';
 	return (dest);
 }
--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define NULL 0
 
 /**
  * _strchr - locate 1st occurrence of char in string and returns pointer there
@@ -11,9 +12,10 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
+	while (s[i] != '\0' && s[i] != c)
+		i++;
 	if (s[i] == c)
-	{
 		return (&s[i]);
-	}
-	return (0);
+	else
+		return (NULL);
 }
