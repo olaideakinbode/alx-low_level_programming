1fcf927e4059cfcc1da734fbe2633e79ba8e6577
olaideakinbode <olaideakinbode@gmail.com>
2023-01-09 12:30:52 -0800

    Static and dynamic Libraries in C

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ * _strchr - locate 1st occurrence of char in string and returns pointer there
+ * @s: string to search
+ * @c: target characer
+ * Return: pointer to that character in string
  */
 
 char *_strchr(char *s, char c)
