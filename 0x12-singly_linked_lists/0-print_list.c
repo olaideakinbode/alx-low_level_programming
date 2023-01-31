99a5e2c7dfae48b77f6e6b319808a5686af2c35b
olaideakinbode <olaideakinbode@gmail.com>
2023-01-31 14:11:07 -0800

    sigly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,16 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t nodelen;
 
-	while (h)
+	nodelen = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		}
-		count++;
+			printf("[%d] %s\n", 0, "(nil)");
+		else printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		nodelen++;
 	}
-	return (count);
+	return (nodelen);
 }
