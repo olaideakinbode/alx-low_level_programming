3e74f54191e97aa06e13884104789fe490c767fe
olaideakinbode <olaideakinbode@gmail.com>
2023-01-31 13:26:23 -0800

    sigly_linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,28 @@
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
-/**
- * myStartupFun - Apply the constructor attribute to my
- * StartupFun() so that it
- * is executed before main()
- */
+ /**
+  * struct list_s - singly linked list
+  * @str: string - (malloc'ed string)
+  * @len: length of the str
+  * @next: points to the next node
+  * Description: singly linked list node structure
+  * for Holberton project
+  */
 
-void myStartupFun(void) __attribute__ ((constructor));
-
-/**
- * myStartupFun - implementation of myStartupFun
- */
-
-void myStartupFun(void)
+typedef struct list_s
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 }
+
+list_t;
+size_t print_list(const list_t *h);
+
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
