351a16ab2d03700acd5b5e31158362639a7da6ce
olaideakinbode <olaideakinbode@gmail.com>
2023-01-31 14:03:40 -0800

    sigly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,49 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
- * Return: void
+ * bmain - function executed before main
+ * Return: no return
  */
 
-void print_opcodes(char *a, int n)
+void __attribute__((constructor)) bmain()
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- * Return: always O
- */
-
-int main(int argc, char **argv)
-{
-	int n;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	n = atoi(argv[1]);
-
-	if (n < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	print_opcodes((char *)&main, n);
-	return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
