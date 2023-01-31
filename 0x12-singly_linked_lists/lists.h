d48debb1d8b650c6e3ff89ea91bedce01d058082
olaideakinbode <olaideakinbode@gmail.com>
2023-01-31 14:21:13 -0800

    sigly_linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
- /**
-  * @struct list_s - singly linked list
-  * @str: string - (malloc'ed string)
-  * @len: length of the str
-  * @next: points to the next node
-  * Description: singly linked list node structure
-  * for Holberton projecti
-  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ *
+ * @next: points to the next node
+ * Description: All prototypes used in the singly linked directory
+ * are contained here and is part of the necessities for succesfull
+ * program compilation.
+ */
 
 typedef struct list_s
 {
