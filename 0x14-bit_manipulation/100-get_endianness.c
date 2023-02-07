2d5adeecaff24739a5fb649e18490a7d0fd90ae3
olaideakinbode <olaideakinbode@gmail.com>
2023-02-07 02:53:31 -0800

    Task_100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- *
- *  *get_endianness - this function checks foe endianness
- *
- *   *Return: 0 if big endian and 1 if little endian
- *
- *    */
+ * get_endianness - this function checks foe endianness
+ * Return: 0 if big endian and 1 if little endian
+ */
 
 int get_endianness(void)
 {
